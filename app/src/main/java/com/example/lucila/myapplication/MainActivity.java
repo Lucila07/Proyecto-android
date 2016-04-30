626f6f34aa1be386584659b99a5c5d31113177d9
aguskoll <aguskoll@gmail.com>
2016-04-30 10:22:23 -0300

    prueba 4

--- app/src/main/java/com/example/lucila/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lucila/myapplication/MainActivity.java
@@ -4,9 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
- //prueba de push koll
- //prueba desp de actualizar
-    //[prtueba 33333
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
