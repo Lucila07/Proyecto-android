023dd3aff776f509d366076b49848c80ec1bc8d8
aguskoll <aguskoll@gmail.com>
2016-06-25 08:56:42 -0300

    key de debug

--- app/src/main/java/com/example/lucila/myapplication/ReservasUsuarioActivity.java
+++ app/src/main/java/com/example/lucila/myapplication/ReservasUsuarioActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
